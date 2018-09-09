ca2da08d8f49ec37ee2606a697276ddc1d7b3378
weiguanghe <334064034@qq.com>
2018-09-09 18:28:17 +0000

    api/users 返回值添加 meta部分

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -33,6 +33,11 @@ class UsersController extends Controller
         \Cache::forget($request->verification_key);
 
         return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
             ->setStatusCode(201);
     }
 
