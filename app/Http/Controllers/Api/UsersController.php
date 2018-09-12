cfb2dde2227983f63619151f28b9d98d9ecdda59
weiguanghe <334064034@qq.com>
2018-09-12 15:28:21 +0000

    actived users

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -57,6 +57,11 @@ class UsersController extends Controller
         return $this->response->item($user, new UserTransformer());
     }
 
+    public function activedIndex(User $user)
+    {
+        return $this->response->collection($user->getActiveUsers(), new UserTransformer());
+    }
+
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,9 @@ $api->version('v1', [
         // 资源推荐
         $api->get('links', 'LinksController@index')
             ->name('api.links.index');
+        // 活跃用户
+        $api->get('actived/users', 'UsersController@activedIndex')
+            ->name('api.actived.users.index');
         // 获取话题详情
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
